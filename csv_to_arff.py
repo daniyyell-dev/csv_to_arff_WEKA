4ad1b3caf00f3b0b8c1837f477a276dc96aeb9ee
daniyyell <121419973+daniyyell-dev@users.noreply.github.com>
2023-12-21 13:38:45 +0100

    Update csv_to_arff.py

--- csv_to_arff.py
+++ csv_to_arff.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from scipy.io import arff
+from tqdm import tqdm
 
 # Load the CSV file
 csv_file_path = 'YOUR_DATASET.csv'  # Replace with the actual path to your CSV file, please makes sure it has correct label e.g BENIGN,MALWARE
@@ -20,3 +21,8 @@ with open(arff_file_path, 'w') as arff_file:
     arff_file.write('\n@DATA\n')
     for index, row in df.iterrows():
         arff_file.write(','.join(map(str, row)) + '\n')
+
+  # Add tqdm to show a progress bar
+    for index, row in tqdm(df.iterrows(), total=len(df), desc="Converting to ARFF...", bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]'):
+        arff_file.write(','.join(map(str, row)) + '\n')
+
